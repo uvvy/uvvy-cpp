d1249de3e8e8c28f049eee3b6fc7360617309d83
Berkus <berkus@gmail.com>
2012-10-06 13:22:50 +0300

    [staticanalyzer] Fix null pointer dereference.

--- netsteria/3rdparty/rtaudio-4.0.11/RtAudio.cpp
+++ netsteria/3rdparty/rtaudio-4.0.11/RtAudio.cpp
@@ -167,7 +167,7 @@ RtAudio :: RtAudio( RtAudio::Api api ) throw()
   getCompiledApi( apis );
   for ( unsigned int i=0; i<apis.size(); i++ ) {
     openRtApi( apis[i] );
-    if ( rtapi_->getDeviceCount() ) break;
+    if ( rtapi_ and rtapi_->getDeviceCount() ) break;
   }
 
   if ( rtapi_ ) return;
