7c56d89446db4db3b5312da98bfbbb4cbead438b
Alexandr Kutuzov <aku@arfa.(none)>
2012-10-29 01:07:52 +0400

    mac support for dock

---
+++ netsteria/gui/macsupport.mm
@@ -0,0 +1,48 @@
+#include "macsupport.h"
+#include <QWidget>
+
+#import <Cocoa/Cocoa.h>
+#import <objc/runtime.h>
+
+static MacSupport *ms_instance = 0;
+
+NSString * nsStringFromQString(const QString & s)
+{
+    const char * utf8String = s.toUtf8().constData();
+    return [[NSString alloc] initWithUTF8String: utf8String];
+}
+
+void dockClickHandler(id self, SEL _cmd)
+{
+    Q_UNUSED(self);
+    Q_UNUSED(_cmd);
+    ms_instance->emitDockClick();
+}
+
+MacSupport::MacSupport() {
+    Class cls = [[[NSApplication sharedApplication] delegate] class];
+
+    if (!class_addMethod(cls, @selector(applicationShouldHandleReopen:hasVisibleWindows:), (IMP) dockClickHandler, "v@:"))
+        NSLog(@"MyPrivate::MyPrivate() : class_addMethod failed!");
+
+    ms_instance = this;
+}
+
+MacSupport::~MacSupport() {
+}
+
+void MacSupport::emitDockClick() {
+    emit dockClicked();
+}
+
+void MacSupport::setDockBadge(const QString & badgeText)
+{
+    NSString * badgeString = nsStringFromQString(badgeText);
+    [[NSApp dockTile] setBadgeLabel: badgeString];
+    [badgeString release];
+}
+
+void MacSupport::requestAttention()
+{
+    [NSApp requestUserAttention: NSInformationalRequest];
+}
