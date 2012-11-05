19546f1f239de4ac75b973cfa49a54a8ec160521
Berkus <berkus@gmail.com>
2012-11-05 12:02:47 +0200

    Add notification signals to filesync watcher. Scan hidden files too.

--- netsteria/lib/filesync.cpp
+++ netsteria/lib/filesync.cpp
@@ -12,15 +12,46 @@ FileSync::FileSync(QObject* parent)
 		this, SLOT(fileChanged(const QString&)));
 	connect(watcher, SIGNAL(directoryChanged(const QString&)),
 		this, SLOT(directoryChanged(const QString&)));
-	watcher->addPath(shareDir.path());
+
+	watchPath(shareDir, true);
+}
+
+void FileSync::watchPath(const QDir& path, bool isDir)
+{
+
+	watcher->addPath(path.path());
+	QDir d(path);
+	d.setFilter(QDir::AllDirs|QDir::NoSymLinks|QDir::NoDotAndDotDot|QDir::Hidden);
+	QStringList dirs = d.entryList();
+	foreach(QString dir, dirs)
+	{
+		QString dpath = path.path() + "/" + dir;
+		qDebug() << "Adding watch on directory" << dpath;
+		watcher->addPath(dpath);
+	}
+
+	d.setFilter(QDir::Files|QDir::Hidden);
+	QStringList files = d.entryList();
+	foreach(QString file, files)
+	{
+		if (file == ".DS_Store")
+			continue;
+		QString fpath = path.path() + "/" + file;
+		qDebug() << "Adding watch on file" << fpath;
+		watcher->addPath(fpath);
+	}
 }
 
 void FileSync::fileChanged(const QString& path)
 {
 	qDebug() << "FILE CHANGE NOTIFY IN" << path;
+	QStringList origFiles = watcher->files();
+	watchPath(QDir(path), false);
 }
 
 void FileSync::directoryChanged(const QString& path)
 {
 	qDebug() << "DIRECTORY CHANGE NOTIFY IN" << path;
+	QStringList origDirs = watcher->directories();
+	watchPath(QDir(path), true);
 }
--- netsteria/lib/filesync.h
+++ netsteria/lib/filesync.h
@@ -2,6 +2,7 @@
 
 #include <QObject>
 
+class QDir;
 class QFileSystemWatcher;
 
 /**
@@ -13,8 +14,48 @@ class FileSync : public QObject
 
 	QFileSystemWatcher* watcher;
 
+	void watchPath(const QDir& path, bool isDir);
+
 public:
 	FileSync(QObject* parent = 0);
+
+signals:
+	/**
+	 * A file has been added under watched tree.
+	 * Signal may be emitted several times for a given path.
+	 * @param path Full path to the added file.
+	 */
+	void fileAdded(const QString& path);
+	/**
+	 * A file has been removed under watched tree.
+	 * Signal may be emitted several times for a given path.
+	 * @param path Full path to the removed file.
+	 */
+	void fileRemoved(const QString& path);
+	/**
+	 * A file contents or metadata has been modified under watched tree.
+	 * Signal may be emitted several times for a given path.
+	 * @param path Full path to the modified file.
+	 */
+	void fileModified(const QString& path);
+	/**
+	 * A directory has been added under watched tree.
+	 * Signal may be emitted several times for a given path.
+	 * @param path Full path to the added directory.
+	 */
+	void directoryAdded(const QString& path);
+	/**
+	 * A directory has been removed under watched tree.
+	 * Signal may be emitted several times for a given path.
+	 * @param path Full path to the removed directory.
+	 */
+	void directoryRemoved(const QString& path);
+	/**
+	 * A directory contents or metadata has been modified under watched tree.
+	 * Signal may be emitted several times for a given path.
+	 * @param path Full path to the modified directory.
+	 */
+	void directoryModified(const QString& path);
 
 private slots:
 	void fileChanged(const QString& path);
