9d5d7d3a95251d7205bf69d4606f8d3303c1a9cd
Berkus <berkus@gmail.com>
2012-11-24 10:48:54 +0200

    Cleanup code, move from inside a namespace.

--- nat/upnp/httprequest.cpp
+++ nat/upnp/httprequest.cpp
@@ -1,35 +1,18 @@
 /***************************************************************************
+ * Based on KTorrent UPnP code by:                                         *
  *   Copyright (C) 2005-2007 by Joris Guisson                              *
  *   joris.guisson@gmail.com                                               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.             *
+ *   Licensed under GPL2+                                                  *
  ***************************************************************************/
+
 #include <QTimer>
 #include <QHostAddress>
 #include <QHttpResponseHeader>
 #include <QStringList>
-#include "httprequest.h"
-
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
-
 #include <QHostInfo>
-
-namespace bt
-{
+#include "httprequest.h"
 
 HTTPRequest::HTTPRequest(const QNetworkRequest& rq, const QString& payld, bool verbose)
     : req(rq)
@@ -110,6 +93,4 @@ void HTTPRequest::onTimeout()
     // sock->close();
     error = tr("Operation timed out");
     result(this);
-}
-
 }
--- nat/upnp/httprequest.h
+++ nat/upnp/httprequest.h
@@ -1,40 +1,20 @@
 /***************************************************************************
+ * Based on KTorrent UPnP code by:                                         *
  *   Copyright (C) 2005-2007 by Joris Guisson                              *
  *   joris.guisson@gmail.com                                               *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.          *
+ *   Licensed under GPL2+                                                  *
  ***************************************************************************/
-#ifndef KTHTTPREQUEST_H
-#define KTHTTPREQUEST_H
+
+#pragma once
 
 #include <QNetworkRequest>
-#include <stdint.h>
 
 class QNetworkAccessManager;
 class QNetworkReply;
 
-namespace bt
-{
-
 /**
- * @author Joris Guisson
- * 
- * Simple HTTP request class. 
-
- * TODO: use QNetworkAccessManager/QNetworkReply for this??!?
+ * @author Joris Guisson / Berkus
+ * Simple HTTP request class using QNetworkAccessManager. 
  */
 class HTTPRequest : public QObject
 {
@@ -97,7 +77,3 @@ private:
     QByteArray reply;
     QString error;
 };
-
-}
-
-#endif
