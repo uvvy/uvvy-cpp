85c50837c003bbf76b7acacc668091f7a2829cc9
Berkus <berkus@gmail.com>
2013-03-03 17:37:07 +0200

    Fix peer name editing.

--- ui/gui/main.h
+++ ui/gui/main.h
@@ -20,15 +20,15 @@ class ChatDialog;
 class PeerTable;
 
 // Columns in PeerTable
-#define NCOLS		5
+#define NCOLS		6
 // default ones
 #define COL_NAME    0
 #define COL_EID     1
 // custom
-#define COL_ONLINE  0 /* In the same column as name, but as a status icon */
-#define COL_FILES   2
-#define COL_TALK	3
-#define COL_LISTEN	4
+#define COL_ONLINE  2
+#define COL_FILES   3
+#define COL_TALK	4
+#define COL_LISTEN	5
 
 class MainWindow : public QMainWindow
 {
