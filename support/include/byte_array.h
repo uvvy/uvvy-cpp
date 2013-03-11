efcac9615eeb739b46c3c2998faca091090ca2d4
Berkus <berkus@gmail.com>
2013-03-11 14:06:28 +0200

    Add byte_array implementation.

--- support/include/byte_array.h
+++ support/include/byte_array.h
@@ -8,16 +8,18 @@
  */
 class byte_array
 {
-	std::vector<uint8_t> value; ///< @todo: make implicitly shared cow?
+	friend bool operator ==(const byte_array& a, const byte_array& b);
+	std::vector<char> value; ///< @todo: make implicitly shared cow?
 public:
 	byte_array();
 	byte_array(const byte_array&);
-	byte_array(const std::vector<uint8_t>&);
-	byte_array(const char*, size_t);
+	byte_array(const std::vector<char>&);
+	byte_array(const char* str);
+	byte_array(const char* data, size_t size);
 	~byte_array();
-	byte_array(byte_array&& other)
-		: value(std::move(other.value))
-	{}
+	// byte_array(byte_array&& other)
+	// 	: value(std::move(other.value))
+	// {}
 	byte_array& operator = (const byte_array& other);
 	byte_array& operator = (byte_array&& other);
 
@@ -29,7 +31,9 @@ public:
 	/**
 	 * @sa size(), capacity()
 	 */
-	const size_t length() const;
+	inline const size_t length() const {
+		return size();
+	}
 
 	char at(int i) const;
 	char operator[](int i) const;
@@ -40,7 +44,16 @@ public:
 	 * If the size is specified, resizes the array beforehand.
 	 */
 	byte_array& fill(char ch, int size = -1);
+
+	/**
+	 * Unlike Qt's fromRawData current implementation of
+	 * wrap does not actually wrap the data, it creates
+	 * its own copy. XXX fix it
+	 */
+	static byte_array wrap(const char* data, size_t size);
 };
+
+bool operator ==(const byte_array& a, const byte_array& b);
 
 /**
 struct BigArray{
---
+++ support/lib/byte_array.cpp
@@ -0,0 +1,81 @@
+#include "byte_array.h"
+
+byte_array::byte_array()
+	: value()
+{}
+
+byte_array::byte_array(const byte_array& other)
+	: value(other.value)
+{}
+
+byte_array::byte_array(const std::vector<char>& v)
+	: value(v)
+{}
+
+byte_array::byte_array(const char* str)
+	: value(str, str+strlen(str)+1)
+{}
+
+byte_array::byte_array(const char* data, size_t size)
+	: value(data, data+size)
+{}
+
+byte_array::~byte_array()
+{}
+
+byte_array& byte_array::operator = (const byte_array& other)
+{
+	if (&other != this)
+	{
+		value = other.value;
+	}
+	return *this;
+}
+
+byte_array& byte_array::operator = (byte_array&& other)
+{
+	return *this;
+}
+
+const char* byte_array::data() const
+{
+	return &value[0];
+}
+
+const size_t byte_array::size() const
+{
+	return value.size();
+}
+
+char byte_array::at(int i) const
+{
+	return value.at(i);
+}
+
+char byte_array::operator[](int i) const
+{
+	return value[i];
+}
+
+char& byte_array::operator[](int i)
+{
+	return value[i];
+}
+
+byte_array& byte_array::fill(char ch, int size)
+{
+	if (size != -1)
+		value.resize(size);
+	std::fill(value.begin(), value.end(), ch);
+	return *this;
+}
+
+byte_array byte_array::wrap(const char* data, size_t size)
+{
+	return byte_array(data, size);
+}
+
+bool operator ==(const byte_array& a, const byte_array& b)
+{
+	return a.value == b.value;
+}
