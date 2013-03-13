e51636817fb2942f5ab9951c081b3c771a884fdb
Berkus <berkus@gmail.com>
2013-03-13 09:25:16 +0200

    Add asio udp sending and receiving.

--- ssu/include/link.h
+++ ssu/include/link.h
@@ -9,12 +9,20 @@ namespace ssu {
 
 class link;
 
+typedef uint32_t magic_t; ///< Protocol magic marker, must have 0x00 as the highest byte (channel number).
 typedef uint8_t channel_number;
+/**
+ * Currently only UDP endpoints/sockets are supported.
+ * System implementation might also have to work over
+ * IP or even Ethernet endpoints - this will require
+ * architectural change.
+ */
+typedef boost::asio::ip::udp::endpoint endpoint;
 
 /**
  * Add an association with particular link to the endpoint.
  */
-class link_endpoint : public boost::asio::ip::udp::endpoint
+class link_endpoint : public endpoint
 {
     std::weak_ptr<link> link_;
 
@@ -33,16 +41,23 @@ class link_endpoint : public boost::asio::ip::udp::endpoint
  */
 class link_channel
 {
+public: // Provide access to signal types for clients
+    typedef boost::signals2::signal<void (byte_array&, const link_endpoint&)> on_received;
+    typedef boost::signals2::signal<void ()> on_ready_transmit;
+
+    on_received received;
+    // Signalled when flow/congestion control may allow new transmission
+    on_ready_transmit ready_transmit;
+
+private:
     std::weak_ptr<link> link_; ///< Link we're currently bound to, if any.
     bool active;               ///< True if we're sending and accepting packets.
-
-    boost::signals2::signal<void (byte_array&, const link_endpoint&)> received;
-    // Signalled when flow/congestion control may allow new transmission
-    boost::signals2::signal<void ()> ready_transmit;
 };
 
 /**
  * Control protocol receiver.
+ * Provides support for receiving control messages for
+ * registered protocol types.
  */
 class link_receiver
 {
@@ -54,7 +69,8 @@ class link_receiver
  */
 class link_host_state
 {
-    virtual link* new_link();
+    virtual link* create_link();
+    virtual link_receiver* receiver(magic_t magic);
 };
 
 /**
@@ -66,13 +82,29 @@ class link_host_state
 class link
 {
     link_host_state* const host;
-
     std::map<std::pair<link_endpoint, channel_number>, link_channel*> channels;
-
     bool active_;
 
 public:
+    // ssu expresses current link status as one of three states:
+    // - up: apparently alive, all's well as far as we know.
+    // - stalled: briefly lost connectivity, but may be temporary.
+    // - down: definitely appears to be down.
+    enum class status {
+        down,
+        stalled,
+        up
+    };
+
     link(link_host_state* h) : host(h) {}
+
+protected:
+    /**
+     * Implementation subclass calls this method with received packets.
+     * @param msg the packet received.
+     * @param src the source from which the packet arrived.
+     */
+    void receive(byte_array& msg, const link_endpoint& src);
 };
 
 /**
@@ -83,6 +115,22 @@ public:
 class udp_link : public link
 {
     boost::asio::ip::udp::socket udp_socket;
+    byte_array received_buffer;
+    endpoint received_from;
+
+public:
+    bool bind(const endpoint& ep);
+    /// Send a packet on this UDP socket.
+    bool send(const endpoint& ep, const char *data, int size);
+    inline bool send(const endpoint& ep, const byte_array& msg) {
+        return send(ep, msg.const_data(), msg.size());
+    }
+
+    /// Return all known local endpoints referring to this link.
+    std::vector<endpoint> local_endpoints();
+
+private:
+    void udp_ready_read(const boost::system::error_code& error, std::size_t bytes_transferred);
 };
 
 } // namespace ssu
