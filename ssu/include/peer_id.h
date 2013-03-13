d719b1cb480ff3be86cea214442c46e75adf4d3e
Berkus <berkus@gmail.com>
2013-03-13 09:24:02 +0200

    Inline all peer_id methods.

--- ssu/include/peer_id.h
+++ ssu/include/peer_id.h
@@ -8,19 +8,22 @@ namespace ssu {
 /**
  * Peer ID - helper for keeping all peer-related ID conversions in one place.
  * Contains a binary peer identifier plus methods to convert it into a string representation.
+ *
+ * Peer ID is a cryptographic identifier for the peer endpoint, it contains the type and keying method
+ * as well as public key fingerprint of the peer.
  */
 class peer_id
 {
     byte_array id_;
 
 public:
-    peer_id() : id_() {} 
-    peer_id(std::string base32) : id_(encode::from_base32(base32)) {}
-    peer_id(byte_array id) : id_(id) {}
+    inline peer_id() : id_() {} 
+    inline peer_id(std::string base32) : id_(encode::from_base32(base32)) {}
+    inline peer_id(byte_array id) : id_(id) {}
 
-    byte_array id() const { return id_; }
-    std::string to_string() const { return encode::to_base32(id_); }
-    bool is_empty() const { return id_.is_empty(); }
+    inline byte_array id() const { return id_; }
+    inline std::string to_string() const { return encode::to_base32(id_); }
+    inline bool is_empty() const { return id_.is_empty(); }
 };
 
 inline bool operator == (const peer_id& a, const peer_id& b) { return a.id() == b.id(); }
