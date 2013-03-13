d2413fef41158bf8269a650c6405b4ed50c37c43
Berkus <berkus@gmail.com>
2013-03-14 00:05:29 +0200

    Log date, time and thread id.

--- support/include/logging.h
+++ support/include/logging.h
@@ -2,12 +2,18 @@
 
 #include <iostream>
 #include <mutex>
+#include <thread>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 class debug
 {
 public:
 	static std::mutex m;
-	debug() { m.lock(); }
+	debug() {
+		m.lock();
+		boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+		std::clog << "[D] " << boost::posix_time::to_simple_string(now).c_str() << " T#" << std::this_thread::get_id() << ' ';
+	}
 	~debug() { std::clog << std::endl; m.unlock(); }
 	template <typename T>
 	std::ostream& operator << (const T& v) { std::clog << v; return std::clog; }
