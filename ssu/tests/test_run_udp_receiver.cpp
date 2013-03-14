efabeab311475a4f2c217bd74c58899b33f943ae
Berkus <berkus@gmail.com>
2013-03-14 23:18:46 +0200

    Send a test UDP message for self.

--- ssu/tests/test_run_udp_receiver.cpp
+++ ssu/tests/test_run_udp_receiver.cpp
@@ -5,8 +5,10 @@ int main()
 	try
 	{
 		ssu::link_host_state state;
+		ssu::endpoint local_ep(boost::asio::ip::udp::v4(), 9660);
 		boost::asio::io_service io_service;
-		ssu::udp_link l(io_service, ssu::endpoint(boost::asio::ip::udp::v4(), 9660), state);
+		ssu::udp_link l(io_service, local_ep, state);
+		l.send(local_ep, "\3\2\1\0hi!", 8);
 		io_service.run();
 	}
 	catch (std::exception& e)
