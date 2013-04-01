c4fb84f3151a406dc8dadef5b8c08c5b9a45079b
Berkus <berkus@gmail.com>
2013-04-01 22:25:40 +0300

    Crypto code by Aldrin D'Souza under liberal 2-clause BSD license.
    Credit where credit is due: taken from Aldrin's Notebook and a nice
    accompanying article at http://aldrin.co/crypto-primitives.html

--- ssu/include/crypto.h
+++ ssu/include/crypto.h
@@ -1,10 +1,31 @@
 ///
-/// Copyright (c) 2013, Aldrin's Notebook.
+/// Copyright (c) 2013, Aldrin D'Souza
+/// All rights reserved. 
+/// http://opensource.org/licenses/BSD-2-Clause
+/// 
+/// Redistribution and use in source and binary forms, with or without modification, are permitted
+/// provided that the following conditions are met:
+/// 
+/// Redistributions of source code must retain the above copyright notice, this list of conditions and
+/// the following disclaimer.
+/// 
+/// Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+/// and the following disclaimer in the documentation and/or other materials provided with the
+/// distribution.
+/// 
+/// THIS SOFTWARE IS PROVIDED BY {{THE COPYRIGHT HOLDERS AND CONTRIBUTORS}} "AS IS" AND ANY EXPRESS OR
+/// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+/// FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL {{THE COPYRIGHT HOLDER OR
+/// CONTRIBUTORS}} BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+/// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+/// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+/// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+/// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///
 /// Taken entirely from https://github.com/aldrin/home/blob/master/code/c++/crypto/crypto.h
+/// and munged to fit.
 ///
-/// License status unknown - might require a rewrite.
-///
-/// Requires OpenSSL.
+/// Requires OpenSSL 1.0.0 or later, install one from brew.
 ///
 #pragma once
 
@@ -17,6 +38,10 @@
 #include <boost/array.hpp>
 #include <boost/utility.hpp>
 #include <boost/asio/buffer.hpp>
+
+#if OPENSSL_VERSION_NUMBER < 0x10000000L
+#error OpenSSL 1.0.0 or later is required for PKCS5_PBKDF2_HMAC and EVP aes-128-gcm functions.
+#endif
 
 // namespace ajd
 // {
@@ -91,7 +116,7 @@
     }
 
     /// Generates a keyed or a plain cryptographic hash.
-    class hash: boost::noncopyable
+    class hash : boost::noncopyable
     {
     public:
       /// A convenience typedef for a 256 bit SHA-256 value.
