40232b86a6fa192bb2266dc4ca645a7ae81c4946
Berkus <berkus@gmail.com>
2013-04-01 23:08:46 +0300

    Run the actual libc++ build.

---
+++ scripts/travis_build_libcxx.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -x
+
+svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
+mkdir -p libcxx/_build_
+cd libcxx/_build_
+# LIBCXX_LIBSUPCXX_INCLUDE_PATHS will break with libstdc++/gcc upgrade.
+CC=clang CXX=clang++ cmake -G "Unix Makefiles" -DLIBCXX_CXX_ABI=libsupc++ -DLIBCXX_LIBSUPCXX_INCLUDE_PATHS="/usr/include/c++/4.6;/usr/include/c++/4.6/x86_64-linux-gnu/" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr .. || exit 1
+make -j3 || exit 1
+sudo make install || exit 1
