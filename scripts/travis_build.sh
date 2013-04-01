3c93f0472b6efa5e940ab84418463dbc3d90c7f5
Berkus <berkus@gmail.com>
2013-04-01 23:58:26 +0300

    Force Debug build on CI.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -4,6 +4,6 @@ set -x
 
 mkdir -p _build_
 cd _build_
-cmake -DBUILD_TESTING=YES -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode .. || exit 1
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode .. || exit 1
 make -j3 install || exit 1
 ctest || exit 1
