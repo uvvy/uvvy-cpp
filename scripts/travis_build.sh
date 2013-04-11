10240d0087bf14bd8ea1edb32cd9a0df3bc21960
Berkus <berkus@gmail.com>
2013-04-11 12:02:04 +0300

    Add some support flags for Travis CI builds.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -4,6 +4,6 @@ set -x
 
 mkdir -p _build_
 cd _build_
-cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode .. || exit 1
+cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode .. || exit 1
 make -j3 install || exit 1
 ctest || exit 1
