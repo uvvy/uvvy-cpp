11fb0aa4528e3db219dcd6f13b18a8d22e002522
Berkus <berkus@gmail.com>
2013-04-28 02:46:04 +0300

    Build opus-streaming test.

--- playground/opus-streaming.cpp
+++ playground/opus-streaming.cpp
@@ -6,86 +6,25 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include <queue>
+#include <mutex>
 #include "link.h"
-#include <opus.h>
-
-class audio_sender;
-
-class audio_hardware
-{
-    RtAudio* audio_inst{0};
-    audio_sender* sender_{0};
-
-    audio_hardware(audio_sender* sender)
-        : sender_(sender)
-    {
-        try {
-            audio_inst  = new RtAudio();
-        }
-        catch (RtError &error) {
-            logger::warning() << "Can't initialize RtAudio library, " << error.what();
-            return;
-        }
-
-        // Open the audio device
-        RtAudio::StreamParameters inparam, outparam;
-        inparam.deviceId = 0;
-        inparam.nChannels = audio_sender::nChannels;
-        outparam.deviceId = 0;
-        outparam.nChannels = audio_receiver::nChannels;
-        unsigned int bufferFrames = minframesize;
-
-        try {
-            audio_inst->openStream(&outparam, &inparam, RTAUDIO_FLOAT32, maxrate, &bufferFrames, rtcallback);
-        }
-        catch (RtError &error) {
-            logger::warning() << "Couldn't open stream, " << error.what();
-            return;
-        }
-
-        hwrate = maxrate;
-        hwframesize = bufferFrames;
-
-        logger::debug() << "Open resulting hwrate " << hwrate << ", framesize " << hwframesize;
-
-        audio_inst->startStream();
-    }
-
-    ~audio_hardware()
-    {
-        try {
-            audio_inst->closeStream();
-        }
-        catch (RtError &error) {
-            logger::warning() << "Couldn't close stream, " << error.what();
-        }
-        delete audio_inst; audio_inst = 0;
-    }
-
-    int rtcallback(void *outputBuffer, void *inputBuffer, unsigned int nFrames, double, RtAudioStreamStatus, void *)
-    {
-        // A PortAudio "frame" is one sample per channel,
-        // whereas our "frame" is one buffer worth of data (as in Speex).
-        assert(nFrames == hwframesize);
-
-        if (inputBuffer) {
-            sender_->send_packet((float*)inputBuffer, nFrames);
-        }
-        if (outputBuffer) {
-            mixout((float*)outputBuffer);
-        }
-
-        return 0;
-    }
-};
+#include "protocol.h"
+#include "logging.h"
+#include "opus.h"
+#include "RtAudio.h"
 
 // Receive packets from remote
 // Opus-decode and playback
-class audio_receiver : public link_receiver
+class audio_receiver : public ssu::link_receiver
 {
     OpusDecoder *decstate{0};
     int framesize{0}, rate{0};
-    constexpr int nChannels = 1;
+    std::mutex queue_mutex;
+    std::queue<byte_array> packet_queue;
+
+public:
+    static constexpr int nChannels = 1;
 
     audio_receiver()
     {
@@ -104,20 +43,36 @@ class audio_receiver : public link_receiver
     }
 
     /* Get and return a packet, decoding it; decode a missing frame if queue is empty */
-    void get_packet()
+    byte_array get_packet()
     {
-        unsigned int len = opus_decode_float(decstate, (unsigned char*)msg.data(), msg.size(), samplebuf, framesize, /*decodeFEC:*/1);
-        // Q_ASSERT(len > 0);
-        // Q_ASSERT(len == frameSize());
+        std::unique_lock<std::mutex> lock(queue_mutex);
+        byte_array decoded_packet(framesize*sizeof(float));
 
-        // "decode" a missing frame
-        // int len = opus_decode_float(decstate, NULL, 0, samplebuf, frameSize(), /*decodeFEC:*/1);
+        if (packet_queue.size() > 0)
+        {
+            byte_array pkt = packet_queue.front();
+            packet_queue.pop();
+            lock.unlock();
+            int len = opus_decode_float(decstate, (unsigned char*)pkt.data()+4, pkt.size()-4, (float*)decoded_packet.data(), framesize, /*decodeFEC:*/1);
+            assert(len > 0);
+            assert(len == framesize);
+        } else {
+            lock.unlock();
+            // "decode" a missing frame
+            int len = opus_decode_float(decstate, NULL, 0, (float*)decoded_packet.data(), framesize, /*decodeFEC:*/1);
+            assert(len > 0);
+            assert(len == framesize);
+        }
+        return decoded_packet;
     }
 
 protected:
     /* Put received packet into receive queue */
-    void receive(const byte_array& msg, const link_endpoint& src) override
+    virtual void receive(const byte_array& msg, const ssu::link_endpoint& src) override
     {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        // extract payload
+        packet_queue.push(msg);
     }
 };
 
@@ -127,10 +82,15 @@ class audio_sender
 {
     OpusEncoder *encstate{0};
     int framesize{0}, rate{0};
-    constexpr int nChannels = 1;
     ssu::link& link_;
+    ssu::endpoint& ep_;
 
-    audio_sender(ssu::link& l) : link_(l)
+public:
+    static constexpr int nChannels = 1;
+
+    audio_sender(ssu::link& l, ssu::endpoint& e)
+        : link_(l)
+        , ep_(e)
     {
         int error = 0;
         encstate = opus_encoder_create(48000, nChannels, OPUS_APPLICATION_VOIP, &error);
@@ -153,12 +113,88 @@ class audio_sender
     // Called by rtaudio callback to encode and send packet.
     void send_packet(float* buffer, size_t nFrames)
     {
-        int nbytes = opus_encode_float(encstate, samplebuf, framesize, (unsigned char*)buffer, nFrames*sizeof(float));
-        link_.send(local_ep, msg);
+        byte_array samplebuf(nFrames*sizeof(float));
+        opus_int32 nbytes = opus_encode_float(encstate, buffer, framesize, (unsigned char*)samplebuf.data()+4, nFrames*sizeof(float)-4);
+        assert(nbytes > 0);
+        samplebuf.resize(nbytes+4);
+        *reinterpret_cast<ssu::magic_t*>(samplebuf.data()) = ssu::stream_protocol::magic;
+        link_.send(ep_, samplebuf);
     }
 };
 
-void main()
+class audio_hardware
+{
+    RtAudio* audio_inst{0};
+    audio_sender* sender_{0};
+    audio_receiver* receiver_{0};
+
+public:
+    audio_hardware(audio_sender* sender, audio_receiver* receiver)
+        : sender_(sender)
+        , receiver_(receiver)
+    {
+        try {
+            audio_inst  = new RtAudio();
+        }
+        catch (RtError &error) {
+            logger::warning() << "Can't initialize RtAudio library, " << error.what();
+            return;
+        }
+
+        // Open the audio device
+        RtAudio::StreamParameters inparam, outparam;
+        inparam.deviceId = 0;
+        inparam.nChannels = audio_sender::nChannels;
+        outparam.deviceId = 0;
+        outparam.nChannels = audio_receiver::nChannels;
+        unsigned int bufferFrames = 480;
+
+        try {
+            audio_inst->openStream(&outparam, &inparam, RTAUDIO_FLOAT32, 48000, &bufferFrames, rtcallback, this);
+        }
+        catch (RtError &error) {
+            logger::warning() << "Couldn't open stream, " << error.what();
+            return;
+        }
+
+        audio_inst->startStream();
+    }
+
+    ~audio_hardware()
+    {
+        try {
+            audio_inst->closeStream();
+        }
+        catch (RtError &error) {
+            logger::warning() << "Couldn't close stream, " << error.what();
+        }
+        delete audio_inst; audio_inst = 0;
+    }
+
+private:
+    static int rtcallback(void *outputBuffer, void *inputBuffer, unsigned int nFrames, double, RtAudioStreamStatus, void *userdata)
+    {
+        audio_hardware* instance = reinterpret_cast<audio_hardware*>(userdata);
+
+        // A PortAudio "frame" is one sample per channel,
+        // whereas our "frame" is one buffer worth of data (as in Speex).
+
+        if (inputBuffer) {
+            instance->sender_->send_packet((float*)inputBuffer, nFrames);
+        }
+        if (outputBuffer) {
+            byte_array pkt = instance->receiver_->get_packet();
+            // inefficient: should be writing directly into outputBuffer
+            std::copy(pkt.data(), pkt.data()+pkt.size(), (float*)outputBuffer);
+        }
+
+        return 0;
+    }
+};
+
+constexpr ssu::magic_t opus_magic = 0x4f505553;
+
+int main()
 {
     ssu::link_host_state host;
     ssu::endpoint local_ep(boost::asio::ip::udp::v4(), 9660);
@@ -166,11 +202,11 @@ void main()
     ssu::udp_link l(io_service, local_ep, host);
 
     audio_receiver receiver;
-    host.bind_receiver(stream_protocol::magic, &receiver);
+    host.bind_receiver(opus_magic, &receiver);
 
-    audio_sender sender(l);
+    audio_sender sender(l, local_ep);
 
-    audio_hardware hw(&sender); // open streams and start io
+    audio_hardware hw(&sender, &receiver); // open streams and start io
 
     io_service.run();
 }
