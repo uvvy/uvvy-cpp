be8ab4f38fd150444d74a4f903e4806af181718c
Berkus <berkus@gmail.com>
2013-06-14 01:13:56 +0300

    Use exact number of available processors to build.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -2,8 +2,11 @@
 
 set -x
 
+ncpus=$(grep -c processor /proc/cpuinfo|wc -l)
+echo "Building on $ncpus processors"
+
 mkdir -p _build_
 cd _build_
 cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode .. || exit 1
-make -j3 install || exit 1
+make -j$ncpus install || exit 1
 ctest || exit 1
