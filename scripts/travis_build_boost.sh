f425025489f34cb03d2efc7488ee1984595ce85b
Berkus <berkus@gmail.com>
2013-07-12 23:39:23 +0300

    Manually build boost.

--- scripts/travis_build_boost.sh
+++ scripts/travis_build_boost.sh
@@ -4,11 +4,17 @@
 
 set -x
 
-wget http://sourceforge.net/projects/boost/files/boost/1.54.0/boost_1_54_0.tar.bz2/download
+wget -O boost_1_54_0.tar.bz2 http://sourceforge.net/projects/boost/files/boost/1.54.0/boost_1_54_0.tar.bz2/download
 tar xjf boost_1_54_0.tar.bz2
 cd boost_1_54_0
 cat <<EOF > user-config.jam
-using clang-linux : : /usr/bin/clang++ ;
+using clang-linux : : /usr/bin/clang++-3.3 ;
 EOF
-./bootstrap.sh --prefix=/usr --libdir=/usr/lib64 --with-toolset=clang --with-libraries=system,date_time,serialization,program_options --without-icu --without-python
-sudo ./b2 --prefix=/usr --libdir=/usr/lib64 -d2 -j2 --layout=system --user-config=user-config.jam threading=multi install toolset=clang cxxflags=-std=c++11 cxxflags=-stdlib=libc++ cxxflags=-fPIC cxxflags=-arch x86_64 linkflags=-stdlib=libc++ linkflags=-arch x86_64 --with-libraries=system,date_time,serialization,program_options --without-icu --without-python
+./bootstrap.sh --prefix=/usr --libdir=/usr/lib64 \
+	--with-toolset=clang --with-libraries=system,date_time,serialization,program_options \
+	--without-icu
+sudo ./b2 --prefix=/usr --libdir=/usr/lib64 -d2 -j2 --layout=system \
+	--user-config=user-config.jam threading=multi install toolset=clang \
+	cxxflags=-std=c++11 cxxflags=-stdlib=libc++ cxxflags=-fPIC cxxflags=-arch x86_64 \
+	linkflags=-stdlib=libc++ linkflags=-arch x86_64 \
+	--without-python
