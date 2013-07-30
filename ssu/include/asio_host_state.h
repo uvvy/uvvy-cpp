ea202e4f18ca778df8b33a223ad3fd8b6a36de56
Berkus <berkus@gmail.com>
2013-07-30 20:40:57 +0300

    Update doc comments.

--- ssu/include/asio_host_state.h
+++ ssu/include/asio_host_state.h
@@ -16,7 +16,14 @@ protected:
 	boost::asio::io_service io_service;
 
 public:
+	/**
+	 * A blocking call that runs service run loop that services all
+	 * asynchronous interactions.
+	 */
 	inline void run_io_service() { io_service.run(); }
+	/**
+	 * Some components of the SSU stack need access to the I/O service.
+	 */
 	inline boost::asio::io_service& get_io_service() { return io_service; }
 };
 
