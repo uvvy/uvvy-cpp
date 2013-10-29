44d6c0af108cf0ec681b8bfcba148c3c9b1565fa
Berkus <berkus@gmail.com>
2013-10-29 15:09:33 +0200

    Build with clang-3.4 from git. Bump CI build submodules.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -4,6 +4,6 @@ set -x
 
 mkdir -p _build_
 cd _build_
-CC=clang-3.3 CXX=clang++-3.3 cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Ninja" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode -DBOOST_LIBRARYDIR=/usr/lib64 .. || exit 1
+CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Ninja" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode -DBOOST_LIBRARYDIR=/usr/lib64 .. || exit 1
 ninja install || exit 1
 ctest || exit 1
