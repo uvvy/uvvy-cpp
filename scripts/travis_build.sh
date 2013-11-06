91b0ed83fe71d391752f5439af34bee7f450b384
Berkus <berkus@gmail.com>
2013-11-06 23:18:31 +0200

    Run unit tests verbosely.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -6,4 +6,4 @@ mkdir -p _build_
 cd _build_
 CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Ninja" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode -DBOOST_LIBRARYDIR=/usr/lib64 .. || exit 1
 ninja -j1 install || exit 1
-ctest || exit 1
+ctest -V || exit 1
