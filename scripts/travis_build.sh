d7ecc3c04aaf41a2b722f1805fb785e0f5eb054e
Berkus <berkus@gmail.com>
2013-11-06 22:32:55 +0200

    Build with -j1 because Travis VM runs out of memory otherwise.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -5,5 +5,5 @@ set -x
 mkdir -p _build_
 cd _build_
 CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Ninja" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode -DBOOST_LIBRARYDIR=/usr/lib64 .. || exit 1
-ninja install || exit 1
+ninja -j1 install || exit 1
 ctest || exit 1
