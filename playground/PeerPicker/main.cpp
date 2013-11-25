69160c5e11d058600368bb51c61d02929136a247
Berkus <berkus@gmail.com>
2013-11-25 10:23:49 +0200

    Disable verbose logging.

--- playground/PeerPicker/main.cpp
+++ playground/PeerPicker/main.cpp
@@ -1,11 +1,18 @@
 #include <QCoreApplication>
 #include "PeerPicker.h"
+#include "logging.h"
 
 //
 // Main application entrypoint
 //
 int main(int argc, char **argv)
 {
+    bool verbose_debug{false};
+
+    if (!verbose_debug) {
+        logger::set_verbosity(logger::verbosity::info);
+    }
+
     QApplication app(argc, argv);
     PeerPicker *dlg = new PeerPicker;
     dlg->show();
