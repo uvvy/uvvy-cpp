5aea64bc36cbbd316c89412bd186967c2e164bbf
Berkus <berkus@gmail.com>
2013-11-26 15:47:07 +0200

    Add more peer table model member functions.

--- playground/PeerPicker/PeerTableModel.cpp
+++ playground/PeerPicker/PeerTableModel.cpp
@@ -1,6 +1,7 @@
 // encapsulate  and connect it to regservers from settings_provider
 // query all peers or query filtered peer list
 // enter them into a table model
+#include <QVector>
 #include "PeerTableModel.h"
 #include "private/regserver_client.h"
 #include "client_profile.h"
@@ -12,12 +13,62 @@ using namespace std;
 using namespace ssu;
 using namespace uia::routing;
 
+enum {
+    Name = 0,
+    EID = 1,
+    Host = 2,
+    Owner_FirstName = 3,
+    Owner_LastName = 4,
+    Owner_NickName = 5,
+    City = 6,
+    MaxColumns
+};
+
+struct Peer
+{
+    // Basic peer info - always present
+    QString name_;
+    peer_id eid_;
+    client_profile profile_;
+    QHash<QPair<int,int>, QVariant> additional_data_;
+    /**
+     * Item flags for each item.
+     */
+    QVector<Qt::ItemFlags> flags_;
+
+    Peer()
+    {
+        init_flags();
+    }
+    Peer(peer_id eid)
+        : name_(), eid_(eid), profile_()
+    {
+        init_flags();
+    }
+
+    void init_flags()
+    {
+        flags_.resize(MaxColumns);
+        for (auto& f : flags_) {
+            f = Qt::ItemIsEnabled | Qt::ItemIsSelectable;
+        }
+        flags_[0] = Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsEditable;
+    }
+};
+
 class PeerTableModel::Private
 {
 public:
     PeerTableModel* parent_;
     internal::regserver_client client_;
-    QList<pair<peer_id, client_profile>> peers_;
+    QList<Peer> peers_;
+
+    /**
+     * Data defining the table headers, by <column, role>.
+     */
+    QHash<QPair<int,int>, QVariant> headers_;
+
+    // shared_ptr<settings_provider> settings; // Save/load peer list in settings if not nullptr
 
     Private(PeerTableModel* parent, ssu::host *h)
         : parent_(parent)
@@ -56,9 +107,9 @@ public:
         int i = 0;
         foreach (auto& pair, peers_)
         {
-            if (pair.first == peer)
+            if (pair.eid_ == peer)
             {
-                peers_[i].second = profile;
+                peers_[i].profile_ = profile;
                 parent_->updateData(i);
                 break;
             }
@@ -70,7 +121,7 @@ public:
     {
         for (auto peer : peers) {
             logger::debug() << "found_peers " << peer;
-            peers_.insert(0, make_pair(peer, client_profile()));
+            peers_.insert(0, Peer(peer));
             client_.lookup(peer);
         }
     }
@@ -87,19 +138,103 @@ PeerTableModel::PeerTableModel(shared_ptr<ssu::host> h, QObject *parent)
     : QAbstractTableModel(parent)
     , m_pimpl(make_shared<Private>(this, h.get()))
 {
+    // Set up default header labels
+    m_pimpl->headers_.insert(QPair<int,int>(Name, Qt::DisplayRole), tr("Name"));
+    m_pimpl->headers_.insert(QPair<int,int>(EID, Qt::DisplayRole), tr("Host ID"));
+    m_pimpl->headers_.insert(QPair<int,int>(Host, Qt::DisplayRole), tr("Host name"));
+    m_pimpl->headers_.insert(QPair<int,int>(Owner_FirstName, Qt::DisplayRole), tr("First name"));
+    m_pimpl->headers_.insert(QPair<int,int>(Owner_LastName, Qt::DisplayRole), tr("Last name"));
+    m_pimpl->headers_.insert(QPair<int,int>(Owner_NickName, Qt::DisplayRole), tr("Nickname"));
+    m_pimpl->headers_.insert(QPair<int,int>(City, Qt::DisplayRole), tr("City"));
+
     m_pimpl->connect_regservers();
+}
+
+int PeerTableModel::insert(ssu::peer_id const& eid, QString name)
+{
+    int row = rowWithId(eid);
+    if (row >= 0)
+        return row;
+
+    if (name.isEmpty())
+        name = tr("(unnamed peer)");
+
+    row = m_pimpl->peers_.size();
+
+    // Insert the new peer
+    beginInsertRows(QModelIndex(), row, row);
+    insertAt(row, eid, name);
+    endInsertRows();
+
+    // Update our persistent peers list - @todo berkus: umm, friend list management.
+    // writePeers();
+
+    // Signal anyone interested
+    Q_EMIT peerInserted(eid);
+
+    return row;
+}
+
+void PeerTableModel::remove(ssu::peer_id const& eid)
+{
+    int row = rowWithId(eid);
+    if (row < 0) {
+        return;
+    }
+
+    // Remove the peer
+    beginRemoveRows(QModelIndex(), row, row);
+    m_pimpl->peers_.removeAt(row);
+    endRemoveRows();
+
+    // Update our persistent peers list
+    // writePeers();
+
+    // Signal anyone interested
+    Q_EMIT peerRemoved(eid);
+}
+
+QString PeerTableModel::name(int row) const
+{
+    return m_pimpl->peers_[row].name_;
+}
+
+QString PeerTableModel::name(ssu::peer_id const& eid, QString const& defaultName) const
+{
+    int row = rowWithId(eid);
+    if (row < 0)
+        return defaultName;
+    return name(row);
+}
+
+ssu::peer_id PeerTableModel::id(int row) const
+{
+    return m_pimpl->peers_[row].eid_;
+}
+
+int PeerTableModel::rowWithId(ssu::peer_id const& eid) const
+{
+    for (int i = 0; i < m_pimpl->peers_.size(); i++)
+        if (m_pimpl->peers_[i].eid_ == eid)
+            return i;
+    return -1;
+}
+
+int PeerTableModel::count() const
+{
+    return m_pimpl->peers_.size();
 }
 
 int PeerTableModel::rowCount(const QModelIndex &parent) const
 {
     Q_UNUSED(parent);
-    return m_pimpl->peers_.size();
+    return count();
 }
 
 int PeerTableModel::columnCount(const QModelIndex &parent) const
 {
     Q_UNUSED(parent);
-    return 6;
+    return MaxColumns;
 }
 
 QVariant PeerTableModel::data(const QModelIndex &index, int role) const
@@ -112,50 +247,65 @@ QVariant PeerTableModel::data(const QModelIndex &index, int role) const
         return QVariant();
     }
 
-    if (role == Qt::DisplayRole) {
-        auto pair = m_pimpl->peers_.at(index.row());
+    auto peer = m_pimpl->peers_.at(index.row());
 
-        if (index.column() == 0) {
-            return pair.first.to_string().c_str();
+    int column = index.column();
+    if (column == Name and (role == Qt::DisplayRole or role == Qt::EditRole))
+        return peer.name_;
+
+    if (role == Qt::DisplayRole) {
+        if (column == EID) {
+            return peer.eid_.to_string().c_str();
         }
-        else if (index.column() == 1) {
-            return pair.second.host_name().c_str();
+        else if (column == Host) {
+            return peer.profile_.host_name().c_str();
         }
-        else if (index.column() == 2) {
-            return pair.second.owner_firstname().c_str();
+        else if (column == Owner_FirstName) {
+            return peer.profile_.owner_firstname().c_str();
         }
-        else if (index.column() == 3) {
-            return pair.second.owner_nickname().c_str();
+        else if (column == Owner_LastName) {
+            return peer.profile_.owner_lastname().c_str();
         }
-        else if (index.column() == 4) {
-            return pair.second.owner_lastname().c_str();
+        else if (column == Owner_NickName) {
+            return peer.profile_.owner_nickname().c_str();
         }
-        else if (index.column() == 5) {
-            return pair.second.city().c_str();
+        else if (column == City) {
+            return peer.profile_.city().c_str();
         }
     }
-    return QVariant();
+    return peer.additional_data_.value(QPair<int,int>(column, role));
 }
 
 QVariant PeerTableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
-    if (role != Qt::DisplayRole) {
+    if (orientation != Qt::Horizontal)
         return QVariant();
+
+    return m_pimpl->headers_.value(QPair<int,int>(section, role));
+}
+
+bool PeerTableModel::setHeaderData(int section, Qt::Orientation orientation,
+    const QVariant& value, int role)
+{
+    if (orientation != Qt::Horizontal) {
+        return false;
     }
 
-    if (orientation == Qt::Horizontal) {
-        switch (section) {
-            case 0: return tr("EID");
-            case 1: return tr("Host name");
-            case 2: return tr("First name");
-            case 3: return tr("Nickname");
-            case 4: return tr("Last name");
-            case 5: return tr("Address");
-            default:
-                return QVariant();
-        }
+    if (section < 0 || section >= MaxColumns) {
+        return false;
     }
-    return QVariant();
+
+    m_pimpl->headers_.insert(QPair<int,int>(section,role), value);
+    return true;
+}
+
+void PeerTableModel::insertAt(int row, ssu::peer_id const& eid, QString const& name)
+{
+    Peer p;
+    p.name_ = name;
+    p.eid_ = eid;
+
+    m_pimpl->peers_.insert(row, p);
 }
 
 bool PeerTableModel::insertRows(int position, int rows, const QModelIndex &index)
@@ -165,8 +315,8 @@ bool PeerTableModel::insertRows(int position, int rows, const QModelIndex &index
 
     for (int row=0; row < rows; row++)
     {
-        pair<peer_id, client_profile> pair;
-        m_pimpl->peers_.insert(position, pair);
+        Peer peer;
+        m_pimpl->peers_.insert(position, peer);
     }
 
     endInsertRows();
@@ -220,9 +370,31 @@ bool PeerTableModel::setData(const QModelIndex &index, const QVariant &value, in
 
 Qt::ItemFlags PeerTableModel::flags(const QModelIndex &index) const
 {
-    if (!index.isValid()) {
-        return Qt::ItemIsEnabled;
+    int row = index.row();
+    if (row < 0 or row >= count()) {
+        return 0;
     }
 
-    return QAbstractTableModel::flags(index);// | Qt::ItemIsEditable;
+    int col = index.column();
+    if (col < 0 or col >= MaxColumns) {
+        return 0;
+    }
+
+    return m_pimpl->peers_[row].flags_[col];
+}
+
+bool PeerTableModel::setFlags(const QModelIndex &index, Qt::ItemFlags flags)
+{
+    int row = index.row();
+    if (row < 0 or row >= count()) {
+        return false;
+    }
+
+    int col = index.column();
+    if (col < 0 or col >= MaxColumns) {
+        return false;
+    }
+
+    m_pimpl->peers_[row].flags_[col] = flags;
+    return true;
 }
--- playground/PeerPicker/PeerTableModel.h
+++ playground/PeerPicker/PeerTableModel.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <QAbstractTableModel>
+#include <memory>
 
+class settings_provider;
 namespace ssu {
     class host;
     class peer_id;
@@ -29,11 +31,70 @@ public:
     int columnCount(const QModelIndex &parent) const override;
     QVariant data(const QModelIndex &index, int role) const override;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+    bool setHeaderData(int section, Qt::Orientation orientation,
+        const QVariant& value, int role = Qt::EditRole) override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
     bool setData(const QModelIndex &index, const QVariant &value, int role=Qt::EditRole) override;
     bool insertRows(int position, int rows, const QModelIndex &index=QModelIndex()) override;
     bool removeRows(int position, int rows, const QModelIndex &index=QModelIndex()) override;
 
+    int count() const;
+    bool setFlags(const QModelIndex &index, Qt::ItemFlags flags);
+
+    // SSU-relevant model functions.
+    /**
+     * Insert a peer.
+     */
+    int insert(ssu::peer_id const& eid, QString name);
+    /**
+     * Remove a peer.
+     */
+    void remove(ssu::peer_id const& eid);
+    /**
+     * Return the name of a peer by row number.
+     */
+    QString name(int row) const;
+    /**
+     * Return the name of a peer by host ID, defaultName if no such peer.
+     */
+    QString name(ssu::peer_id const& eid, QString const& defaultName = tr("(unnamed peer)")) const;
+    /**
+     * Return the host ID of a peer by row number.
+     */
+    ssu::peer_id id(int row) const;
+    /**
+     * Return the row number of a peer by its host ID, -1 if no such peer.
+     */
+    int rowWithId(ssu::peer_id const& eid) const;
+
+    inline bool containsId(ssu::peer_id const& eid) const {
+        return rowWithId(eid) >= 0;
+    }
+
+    /**
+     * Return a list of all peers' host IDs.
+     */
+    QList<ssu::peer_id> ids() const;
+
+    /**
+     * Set up the PeerTableModel to keep its state persistent
+     * in the specified settings_provider object under name "peers".
+     * This method immediately reads the peer names from the settings,
+     * then holds onto the settings and writes any updates to it.
+     * Must be called when PeerTableModel is freshly created and still empty.
+     */
+    void use_settings(std::shared_ptr<settings_provider> settings);
+
+// private:
     // Internal use only.
     void updateData(int row);
+private:
+    void insertAt(int row, ssu::peer_id const& eid, QString const& name);
+    void write_settings();
+
+signals:
+    // These signals provide slightly simpler alternatives to
+    // beginInsertRow, endInsertRow, beginInsertColumn, endInsertColumn.
+    void peerInserted(ssu::peer_id const& eid);
+    void peerRemoved(ssu::peer_id const& eid);
 };
