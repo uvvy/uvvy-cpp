159961481e0afc9feed38afa0692fb84705611ac
Berkus <berkus@gmail.com>
2014-01-05 14:24:33 +0200

    Move MacSupport to new UI.

--- ui/qt4/macosx.mm
+++ ui/qt4/macosx.mm
@@ -6,6 +6,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
 //
+#include "macsupport.h"
+
+#import <Cocoa/Cocoa.h>
+#import <objc/runtime.h>
 #include <QWidget>
 
 float dpiScaleFactor(QWidget* widget)
@@ -16,4 +20,51 @@ float dpiScaleFactor(QWidget* widget)
         scaleFactor = [[view window] backingScaleFactor];
 
     return scaleFactor;
+}
+
+//=================================================================================================
+// MacSupport
+//=================================================================================================
+
+static MacSupport *ms_instance = 0;
+
+NSString * nsStringFromQString(const QString & s)
+{
+    const char * utf8String = s.toUtf8().constData();
+    return [[NSString alloc] initWithUTF8String: utf8String];
+}
+
+void dockClickHandler(id self, SEL _cmd)
+{
+    Q_UNUSED(self);
+    Q_UNUSED(_cmd);
+    ms_instance->emitDockClick();
+}
+
+MacSupport::MacSupport() {
+    Class cls = [[[NSApplication sharedApplication] delegate] class];
+
+    if (!class_addMethod(cls, @selector(applicationShouldHandleReopen:hasVisibleWindows:), (IMP) dockClickHandler, "v@:"))
+        NSLog(@"MyPrivate::MyPrivate() : class_addMethod failed!");
+
+    ms_instance = this;
+}
+
+MacSupport::~MacSupport() {
+}
+
+void MacSupport::emitDockClick() {
+    emit dockClicked();
+}
+
+void MacSupport::setDockBadge(const QString & badgeText)
+{
+    NSString * badgeString = nsStringFromQString(badgeText);
+    [[NSApp dockTile] setBadgeLabel: badgeString];
+    [badgeString release];
+}
+
+void MacSupport::requestAttention()
+{
+    [NSApp requestUserAttention: NSInformationalRequest];
 }
