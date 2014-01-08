51db847d8b1aa9fe771265643139e0123df4f5c8
Berkus <berkus@gmail.com>
2014-01-08 18:26:08 +0200

    Add MainWindow.

---
+++ ui/qt4/MainWindow.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QModelIndex>
+#include <QSystemTrayIcon>
+
+class QTableView;
+class PeerTableModel;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+    QTableView *peerlist;
+
+public:
+    MainWindow(PeerTableModel* model);
+    ~MainWindow();
+
+protected:
+    // Re-implement to watch for window activate/deactivate events.
+    virtual bool event(QEvent *event);
+    virtual void closeEvent(QCloseEvent *event);
+
+private:
+    int selectedFriend();
+
+private slots:
+    void exitApp();
+    void updateMenus();
+    void friendsClicked(const QModelIndex &index);
+    void trayActivate(QSystemTrayIcon::ActivationReason);
+};
+
--- ui/qt4/main.cpp
+++ ui/qt4/main.cpp
@@ -1,6 +1,9 @@
-#include <QCoreApplication>
-#include "PeerPicker.h"
+#include <QApplication>
+#include "MainWindow.h"
+#include "PeerTableModel.h"
 #include "logging.h"
+#include "host.h"
+#include "settings_provider.h"
 
 //
 // Main application entrypoint
@@ -12,9 +15,14 @@ int main(int argc, char **argv)
     // if (!verbose_debug) {
         // logger::set_verbosity(logger::verbosity::info);
     // }
-
     QApplication app(argc, argv);
-    PeerPicker *dlg = new PeerPicker;
-    dlg->show();
+    app.setQuitOnLastWindowClosed(false);
+
+    std::shared_ptr<ssu::host> host =
+        ssu::host::create(settings_provider::instance());
+    PeerTableModel* model = new PeerTableModel(host);
+    MainWindow* mainWin = new MainWindow(model);
+
+    mainWin->show();
     return app.exec();
 }
