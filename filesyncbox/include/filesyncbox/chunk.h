058c33b2cd6fe9f79a7eb0cf7452dc110c0737f2
Berkus <berkus@gmail.com>
2014-01-10 13:22:48 +0200

    Add filesync helpers skeleton.

---
+++ filesyncbox/include/filesyncbox/chunk.h
@@ -0,0 +1,8 @@
+#pragma once
+
+/**
+ * A chunk is a piece of data identified by a combination of hashes.
+ *
+ * A chunk is "positively matched" when all the hashes match search criteria.
+ * A chunk is "partially matched" when at least one hash matches.
+ */
---
+++ filesyncbox/include/filesyncbox/index.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * Index keeps all chunks in a multimap by all used hashes.
+ * It can quickly find a chunk given any hash.
+ *
+ * Index also allows to look up hash information for a given local file, under any directory.
+ * It will calculate the information upon first request and cache it.
+ */
