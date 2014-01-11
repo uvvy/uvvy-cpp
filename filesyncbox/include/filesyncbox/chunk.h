1fd808f1d3842bf0164990c3fde93c27ff2b7346
Berkus <berkus@gmail.com>
2014-01-11 23:02:11 +0200

    Add chunk declaration.

--- filesyncbox/include/filesyncbox/chunk.h
+++ filesyncbox/include/filesyncbox/chunk.h
@@ -1,8 +1,34 @@
 #pragma once
 
+namespace filesyncbox {
+
 /**
  * A chunk is a piece of data identified by a combination of hashes.
  *
  * A chunk is "positively matched" when all the hashes match search criteria.
  * A chunk is "partially matched" when at least one hash matches.
  */
+class chunk
+{
+public:
+    // outer_hash() const;
+    // inner_hash() const;
+    size_t small_hash() const;
+};
+
+} // filesyncbox namespace
+
+// Hash specialization for chunk
+namespace std {
+
+template<>
+struct hash<filesyncbox::chunk> : public std::unary_function<filesyncbox::chunk, size_t>
+{
+    inline size_t operator()(filesyncbox::chunk const& a) const noexcept
+    {
+        return a.small_hash();
+    }
+};
+
+} // namespace std
+
