9cec46bc79af587ccd3e458d4f14691955ac35a4
Berkus <berkus@gmail.com>
2014-04-07 12:58:15 +0300

    Document chunk sizes.

--- filesyncbox/include/filesyncbox/chunk.h
+++ filesyncbox/include/filesyncbox/chunk.h
@@ -4,13 +4,20 @@ namespace filesyncbox {
 
 /**
  * A chunk is a piece of data identified by a combination of hashes.
+ * Typical chunk sizes are 256k, 512k, 1M, 2M, 4M.
  *
  * A chunk is "positively matched" when all the hashes match search criteria.
  * A chunk is "partially matched" when at least one hash matches.
  */
 class chunk
 {
+    size_t size_factor_; // log2, e.g. 18 is 256k, 19 is 512k, 20 is 1M
+
 public:
+    size_t size() const {
+        return 1 << size_factor_;
+    }
+
     // outer_hash() const;
     // inner_hash() const;
     size_t small_hash() const;
