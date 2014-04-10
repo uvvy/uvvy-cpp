043466eed0211d593b4da648557b9730dabc7505
Berkus <berkus@gmail.com>
2014-04-10 12:13:45 +0300

    Add signals to chunk reader.

--- filesyncbox/include/filesyncbox/abstract_chunk_reader.h
+++ filesyncbox/include/filesyncbox/abstract_chunk_reader.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <boost/signals2/signal.hpp>
+#include "arsenal/byte_array.h"
+
+/**
+ * Base interface for chunk data provider.
+ * It supports only one operation: request chunk of data by its outer hash.
+ * It can respond in one of two ways: by returning the data via on_got_data signal
+ * or by giving up via on_no_data signal. You could also override virtual got_data() and
+ * no_data() in your subclass to provide additional handling.
+ */
 class abstract_chunk_reader
 {
 protected:
@@ -15,10 +25,17 @@ protected:
      */
     void read_chunk(byte_array const& ohash);
 
+    boost::signals2::signal<void (byte_array const&, byte_array const&)> on_got_data;
+    boost::signals2::signal<void (byte_array const&)> on_no_data;
+
     /**
      * The chunk reader calls these methods when a chunk arrives,
      * or when it gives up on finding a chunk.
      */
-    virtual void got_data(byte_array const& outer_hash, byte_array const& data) = 0;
-    virtual void no_data(byte_array const& outer_hash) = 0;
+    virtual void got_data(byte_array const& outer_hash, byte_array const& data) {
+        on_got_data(outer_hash, data);
+    }
+    virtual void no_data(byte_array const& outer_hash) {
+        on_no_data(outer_hash);
+    }
 };
