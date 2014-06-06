fa1a1655b04bad9aa45c6d76eec4568f3b63b6d0
Berkus <berkus@gmail.com>
2014-06-06 18:39:04 +0300

    Update license header addresses.

--- filesyncbox/lib/abstract_chunk_reader.cpp
+++ filesyncbox/lib/abstract_chunk_reader.cpp
@@ -1,3 +1,11 @@
+//
+// Part of Metta OS. Check http://atta-metta.net for latest version.
+//
+// Copyright 2007 - 2014, Stanislav Karchebnyy <berkus@atta-metta.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
+//
 #include "filesyncbox/abstract_chunk_reader.h"
 
 void abstract_chunk_reader::read_chunk(byte_array const& ohash)
--- voicebox/tests/test_sim_transmit.cpp
+++ voicebox/tests/test_sim_transmit.cpp
@@ -1,14 +1,14 @@
 //
-// Part of Metta OS. Check http://metta.exquance.com for latest version.
+// Capture audio locally,
+// Send it through simulated network,
+// Playback locally again.
 //
-// Copyright 2007 - 2014, Stanislav Karchebnyy <berkus@exquance.com>
+// Part of Metta OS. Check http://atta-metta.net for latest version.
+//
+// Copyright 2007 - 2014, Stanislav Karchebnyy <berkus@atta-metta.net>
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See file LICENSE_1_0.txt or a copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-// Capture audio locally
-// Send it through simulated network
-// Playback locally again
 //
 #define BOOST_TEST_MODULE Test_sim_transmit
 #include <boost/test/unit_test.hpp>
