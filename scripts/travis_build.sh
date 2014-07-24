d8ad7a1d946bf76859ff2bc374767553896eb83c
Berkus <berkus@gmail.com>
2014-07-24 15:19:28 +0300

    Bump submodules and Travis configs.

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
+# BUILD_TYPE comes from Travis environment.
 
 set -x
 
 mkdir -p _build_
 cd _build_
-CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTING=YES -G "Ninja" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/mettanode -DBOOST_LIBRARYDIR=/usr/lib64 .. || exit 1
+CC=/usr/bin/clang CXX=/usr/bin/clang++ cmake -DTRAVIS_CI=YES -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DBUILD_TESTING=YES -G "Ninja" -DCMAKE_INSTALL_PREFIX=`pwd`/dist/$BUILD_TYPE/mettanode -DBOOST_LIBRARYDIR=/usr/lib64 .. || exit 1
 ninja -j1 install || exit 1
 
 # byte_array, flurry and crypto unit tests suffer from
