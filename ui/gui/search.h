7bbd555a62eaf9137704e1469b124db9bb8212d7
Berkus Decker <berkus@madfire.net>
2014-11-13 18:39:17 +0200

    Trailing whitespace cleanup.

--- ui/gui/search.h
+++ ui/gui/search.h
@@ -13,39 +13,39 @@ class QLineEdit;
 class QTableView;
 class QTableWidgetItem;
 namespace SST {
-	class RegInfo;
-	class Endpoint;
-	class PeerId;
+    class RegInfo;
+    class Endpoint;
+    class PeerId;
 }
 
 class SearchDialog : public QDialog
 {
-	Q_OBJECT
+    Q_OBJECT
 
 private:
-	typedef SST::Endpoint Endpoint;
-	typedef SST::RegInfo RegInfo;
+    typedef SST::Endpoint Endpoint;
+    typedef SST::RegInfo RegInfo;
 
-	QLineEdit *textline;
-	QTableWidget *results;
+    QLineEdit *textline;
+    QTableWidget *results;
 
-	QString searchtext;
-	QHash<SST::PeerId, int> reqids;
-	QList<SST::PeerId> resultids;
+    QString searchtext;
+    QHash<SST::PeerId, int> reqids;
+    QList<SST::PeerId> resultids;
 
 public:
-	SearchDialog(QWidget *parent);
+    SearchDialog(QWidget *parent);
 
-	void present();
+    void present();
 
 
 private:
-	void closeEvent(QCloseEvent *event);
-	QTableWidgetItem *item(const QString &text);
+    void closeEvent(QCloseEvent *event);
+    QTableWidgetItem *item(const QString &text);
 
 private slots:
-	void startSearch();
-	void searchDone(const QString &text, const QList<SST::PeerId> ids, bool complete);
-	void lookupDone(const SST::PeerId &id, const Endpoint &loc, const RegInfo &info);
-	void addPeer();
+    void startSearch();
+    void searchDone(const QString &text, const QList<SST::PeerId> ids, bool complete);
+    void lookupDone(const SST::PeerId &id, const Endpoint &loc, const RegInfo &info);
+    void addPeer();
 };
